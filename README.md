b4cc2e675f453265f058ffe413ff85d134c71647
root <maniarjunbolllam@gmail.com>
2022-10-25 20:55:54 +0530

    Updated README.md and added app.py,haarcascade_russian_plate_number.xml,requirements.txt,index.html files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# number_plate_detection
+### Cars Number Plate Detection
+
+
+### Software and Tools Requirements
+
+1. [GithubAccount](https://github.com)
+2. [HerokuAccount](https://heroku.com)
+3. [VSCodeIDE](https://code.visualstudio.com/)
+4. [GitCLI](https://git-scm.com/downloads)
+
+
+Create a new environment
+
+```
+conda create -p venv python==3.7 -y
+
+```
+
+Create app.py file & index.html file.
+
+```
+Write the desired code on both files so that the machine learning model runs smoothly and produces the desired results.
+
+```
+
+Deployment
+
+```
+Deploy model on the Heruko platform
+
+```
---
+++ app.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import imutils
+import easyocr
+from PIL import Image
+from flask import Flask,request,app,render_template
+
+app=Flask(__name__)
+
+def text_detection(image):
+  n_plate_detector = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
+  img = np.asarray(image)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  detections = n_plate_detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=7)
+  if len(detections)==1:
+    for (x,y,w,h) in detections:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    plate = img[y:y + h, x:x + w]
+
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(plate)
+    return "The predicted Text is "+result[0][1]
+  else:
+    return "Sorry, model unable to detect Text"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    file = request.files["file"]
+    img = Image.open(file.stream)
+    output=text_detection(img)
+    return render_template('index.html',prediction_text=output)
+
+if __name__=="__main__":
+    app.run(debug=True)
